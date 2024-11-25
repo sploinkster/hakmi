c30610298e12ff26d02427e85f6a9c9930381a94
sploinkster <ja462577@ucf.edu>
2024-11-24 19:46:34 -0500

    Update main.py
    change discord text markdown

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ async def on_message(message):
         result = output.getvalue()
         output.close()
         
-        chunk_len=1900
+        chunk_len=1994
         if len(result) > chunk_len:
             for i in range(0, len(result), chunk_len):
-                await message.channel.send(result[0+i:chunk_len+i])
+                await message.channel.send('```'+result[i:chunk_len+i]+'```')
         else:
-            await message.channel.send(result)
+            await message.channel.send('```'+result+'```')
 
 
 client.run("token goes here")
